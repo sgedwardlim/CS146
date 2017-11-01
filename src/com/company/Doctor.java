b81694d6395470a434906890701b8af1837ad346
richsensations <32314978+richsensations@users.noreply.github.com>
2017-11-01 15:26:47 -0700

    updated doc class

--- src/com/company/Doctor.java
+++ src/com/company/Doctor.java
@@ -1,8 +1,43 @@
 package com.company;
 
 public class Doctor extends People {
+	private String speciality;
 	
-	public Doctor(String name, Gender gender, Integer age){
-		
+	public Doctor(String name, Gender gender, Integer age, String speciality){
+		super(name, gender, age);
+		this.speciality = speciality;
 	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public Gender getGender(){
+		return gender;
+	}
+	
+	public Integer getAge(){
+		return age;
+	}
+	
+	public String getSpeciality(){
+		return speciality;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setGender(Gender gender){
+		this.gender = gender;
+	}
+	
+	public void setAge(Integer age){
+		this.age = age;
+	}
+	
+	public void setSpeciality(String speciality){
+		this.speciality = speciality;
+	}
+	
 }
